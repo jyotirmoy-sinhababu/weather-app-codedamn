a95fc6108d9d6ead1d437de86c7023d873029b70
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-01-20 21:11:01 +0530

    handle submit function created

--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -10,26 +10,31 @@ const MainPage = () => {
   const [data, setData] = useState([]);
   const [inputData, setInputData] = useState('');
 
-  useEffect(() => {
+  const handleSubmit = () => {
     try {
       axios
         .get(
           `https://api.open-meteo.com/v1/forecast
-?latitude=52.52&longitude=13.41
+?latitude=${inputData.lat}.52&longitude=${inputData.lon}
 &current_weather=true`
         )
         .then((res) => {
           console.log(res.data);
-          setData();
+          setData(res.data);
         });
     } catch (err) {
       console.error(err);
     }
-  }, []);
+  };
+  console.log(inputData.lon);
+  console.log(inputData.lat);
+  const handleChange = (e) => {
+    setInputData({ ...inputData, [e.target.name]: e.target.value });
+  };
 
   return (
     <>
-      <InputForm />
+      <InputForm handleChange={handleChange} handleSubmit={handleSubmit} />
     </>
   );
 };
