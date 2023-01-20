f8eab2f92bab73c88ca5020fda6e5556d81ae68e
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-01-20 19:38:29 +0530

    handle change function created

--- src/components/form/InputForm.js
+++ src/components/form/InputForm.js
@@ -1,21 +1,28 @@
 import React from 'react';
 
-const InputForm = () => {
+const InputForm = ({ handleChange }) => {
   return (
     <>
       <form>
         <input
+          onChange={(e) => {
+            handleChange(e);
+          }}
           type='text'
           className='frm-inp-holder'
           name='lat'
           placeholder='latitude'
         />
         <input
+          onChange={(e) => {
+            handleChange(e);
+          }}
           type='text'
           className='frm-inp-holder'
           name='lon'
           placeholder='longitude'
         />
+        <button></button>
       </form>
     </>
   );
